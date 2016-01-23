6222a4743841b9ccb8c3387bcbd8603758e11cf8
fenceFoil <fenceFoil@mailinator.com>
2016-01-22 22:17:03 -0500

    Did stuff

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 #!/bin/python3
-import blinkstick.blinkstick
+import blinkstick.blinkstick as blinkstick
 import pygame
 from pygame.locals import *
 import time
 import numpy
 
-#Blinkstick init
-bs = blinkstick.blinkstick.BlinkStickPro (16, 0, 0, 0.002, 255)
-bs.connect()
-
-#Pygame init
-pygame.init()
-SCREEN = pygame.display.set_mode((500,400),0,32)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 
-
 class Canopto:
 	'The Matrix of LEDs that make up the display'
-	colCount = 2
-	rowCount = 8
-	matrix = numpy.zeros((rowCount,colCount), dtype=(float,3))
+	width = 2
+	height = 8
+	matrix = numpy.zeros((height,width), dtype=(float,3))
 	pixelPadding = 10
-	pyCenter = (int(SCREEN.get_width()/2), int(SCREEN.get_height()/2))
+	# pyCenter = (int(self.self.SCREEN.get_width()/2), int(selfSCREEN.get_height()/2))
 	
-	def __init__(self, colCount, rowCount, bs, pg):
-		print("Initializing...")
-		self.bs = bs
-		self.pg = pg
+	def __init__(self, width, height):
+		
+		#Blinkstick init
+		bs = blinkstick.BlinkStickPro (16, 0, 0, 0.002, 255)
+		bs.connect()
+
+		#Pygame init
+		pygame.init()
+		self.SCREEN = pygame.display.set_mode((500,400),0,32)
+
 	
 	def writeChar(self, character):
 		'Write the character to the display'
@@ -38,31 +36,36 @@ class Canopto:
 		print(self.matrix)
 		
 		#reset pygame window
-		SCREEN.fill(BLACK)
+		self.SCREEN.fill(BLACK)
 		
 		#draw pixels onto pygame window
-		for r in range(0,self.rowCount):
-			for c in range(0,self.colCount):
+		for r in range(0,self.height):
+			for c in range(0,self.width):
 				#~ self.pg
-				pygame.draw.circle(SCREEN, WHITE, (c*20 + 100, r*20 + 100), 10)
+				pygame.draw.circle(self.SCREEN, WHITE, (c*20 + 100, r*20 + 100), 10)
 		#~ self.pg
 		pygame.display.update()
 		
-	def softToHardPixel(self, row, col):
-		'Convert a software pixel (row, col) to the hardware pixel index'
+	def softToHardPixel(self, col, row):
+		'Convert a software pixel (col, row) to the hardware pixel index'
 		print("UGGH")
-		
-	def setPixel(self, row, col, color):
+		# 16 or width*height*(col//2) selects a 2-can-wide column of lights
+		# (((height-1)-row)//4) selects a cell of 4 cans
+		# (row%2) adds one if the row is odd
+		# (col%2) is true for the second column of cans in a cell
+		# (1+((row+1)%2)) adds 1 for the lower right can in a cell, and adds one more for the upper right can in a cell
+		return (self.width*self.height)*(col//2) + (((self.height-1)-row)//2)*4+(row%2)+(col%2)*(3-2*((row)%2))		
+	def setPixel(self, col, row, color):
 		self.matrix[row][col] = color
-		bs.set_color (0, 1, color[0], color[1], color[2])
+		bs.set_color (0, softToHardPixel(col, row), color[0], color[1], color[2])
 		bs.send_data(0)
 
 
-
-
-CANOPTO = Canopto(2, 8, bs, pygame)
-CANOPTO.setPixel(0,0, WHITE)
-while 1:
-	CANOPTO.update()
-	
-	time.sleep(.25)
+#Main
+if __name__ == "__main__":
+	CANOPTO = Canopto(2, 8)
+	CANOPTO.setPixel(0,0, WHITE)
+	while 1:
+		CANOPTO.update()
+		
+		time.sleep(.25)
