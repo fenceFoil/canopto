c10cb02e7f1b07a5e767e6f155d360d06de0536d
fenceFoil <fenceFoil@mailinator.com>
2016-01-23 00:24:45 -0500

    Working on snowflakes

--- shows/Snowflakes01.py
+++ shows/Snowflakes01.py
@@ -9,10 +9,12 @@ import random
 c = Canopto(2, 8)
 
 while True:
-	last_column = 0
+	last_x = 0
 	for y in range(0, 8):
 		if y > 0:
 			# Fade out old position
+			while c.get
+			last_x
 		
 	# Create a 1-pixel white snowflake
 	# Animate it falling down, by moving the snowflake to a random column, and fading out previoud position
